6f6586ff58d751e0209614b7fc3cafada8bcba00
raza241 <raza241@yahoo.com>
2019-03-29 16:12:15 +0500

    remove npm folder

--- assets/src/js/scrollmagic.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var Controller = new ScrollMagic.Controller();
-    var OurScene = new ScrollMagic.Scene({
-        triggerElement : '.heading'
-    })
-    .setClassToggle('#introduction','fadeInUp')
-    .addTo(Controller);
-});
